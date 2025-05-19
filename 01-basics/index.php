aea4e06b372f5a0a5ecd159a6fbb4882697abecf
hafizi <mohamadhafizihamid@gmail.com>
2025-05-20 07:10:23 +0800

    feat: learn concatenate and variables

--- 01-basics/index.php
+++ 01-basics/index.php
@@ -1,2 +1,12 @@
 <?php
 echo "Hello, PHP with Docker!";
+
+# concatenate in PHP
+// use the period (.) as the concatenate operator
+echo "hello . hafizi";
+
+# variables in PHP
+// create using the dollar sign ($)
+$greet = "hello";
+// add space in concacenate
+echo $greet . " everybody";
