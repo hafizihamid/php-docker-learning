544965802162a883a8f1fe9380050204eb8d2a86
hafizi <mohamadhafizihamid@gmail.com>
2025-05-20 20:41:46 +0800

    feat: learn single & double quotes

--- 01-basics/index.php
+++ 01-basics/index.php
@@ -6,7 +6,17 @@ echo "Hello, PHP with Docker!";
 echo "hello . hafizi";
 
 # variables in PHP
-// create using the dollar sign ($)
+// create using the dollar sign ($) 
 $greet = "hello";
+// $greet will hold the "hello" value
+
 // add space in concacenate
 echo $greet . " everybody";
+echo $greet . " " ."everybody";
+// it will output 'hello everybody'
+
+# singles & doubles quotes
+// double allow variable interpolation (variables inside the strings are evaluated)
+// single treat the content literally (variables not evaluated)
+echo "$greet everybody";
+echo '$greet everybody';
